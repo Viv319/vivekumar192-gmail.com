e8540b740a6adefb33ffca64d20fc70e0e27d720
vivek <modanwal319@gmail.com>
2024-07-05 00:35:03 +0530

    puu

--- Application/server/index.js
+++ Application/server/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(cors(
   {
-    origin: ["https://vivekumar192-gmail-com-cuvette-final-evaluation-nov-cne5frnmi.vercel.app"],
+    origin: ["https://vivekumar192-gmail-com-cuvette-fina-git-ab1eff-viv319s-projects.vercel.app"],
     methods:["POST","PUT","PATCH","DELETE","GET"],
     credentials:true
   }
